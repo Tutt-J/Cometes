5945217758e2d0b9c28148e14a9c7f0137356f9a
Tutt-J <contact@digistos.com>
2020-10-25 09:54:19 +0100

    correct semicolon

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -13,8 +13,6 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
-use App\Form\AddressType;
-use Symfony\Component\Validator\Constraints\Choice;
 use Symfony\Component\Validator\Constraints\Email;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
@@ -46,7 +44,7 @@ class UserType extends AbstractType
             'row_attr' => [
                 'class' => 'col-md-6'
             ],
-        ]
+        ];
     }
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
