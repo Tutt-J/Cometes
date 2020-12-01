fa66b649fa1c37e8391cc737381f43cb94f07a25
Tutt-J <contact@digistos.com>
2020-12-01 16:39:02 +0100

    change post controller to only view only post

--- src/Repository/ArticleRepository.php
+++ src/Repository/ArticleRepository.php
@@ -33,7 +33,8 @@ class ArticleRepository extends ServiceEntityRepository
             ->addSelect('RAND() as HIDDEN rand')
             ->andWhere('a.category = :val')
             ->andWhere('a.id != :id')
-            ->setParameters(array('val'=> $current->getCategory(), 'id' => $current->getId()))
+            ->andWhere('a.isOnline = :online')
+            ->setParameters(array('val'=> $current->getCategory(), 'id' => $current->getId(), 'online' => 1))
             ->orderBy('rand')
             ->setMaxResults(3)
             ->getQuery()
@@ -41,18 +42,41 @@ class ArticleRepository extends ServiceEntityRepository
             ;
     }
 
+    public function findPrev($current){
+        return $this->createQueryBuilder('a')
+            ->andWhere('a.id < :id')
+            ->andWhere('a.isOnline = :online')
+            ->setParameters(array( 'id' => $current->getId(), 'online' => 1))
+            ->setMaxResults(1)
+            ->getQuery()
+            ->getResult()
+            ;
+    }
+
+    public function findNext($current){
+        return $this->createQueryBuilder('a')
+            ->andWhere('a.id > :id')
+            ->andWhere('a.isOnline = :online')
+            ->setParameters(array( 'id' => $current->getId(), 'online' => 1))
+            ->setMaxResults(1)
+            ->getQuery()
+            ->getResult()
+            ;
+    }
+
     /**
-     * @param $tag
+     * @param $keyword
      * @return mixed
      */
-    public function findByTag($tag)
+    public function findByKeyword($keyword)
     {
         return $this->createQueryBuilder('a')
             ->select('a')
             ->leftJoin('a.keywords', 'c')
             ->addSelect('c')
             ->andWhere('c.keyword = :c')
-            ->setParameter('c', $tag->getKeyword())
+            ->andWhere('a.isOnline = :online')
+            ->setParameters(array('c' => $keyword->getKeyword(), 'online' => 1))
             ->getQuery()
             ->getResult();
     }
