c29fe66432e6ac0bc0f515d688ae472442bde501
Tutt-J <contact@digistos.com>
2020-12-07 15:02:38 +0100

    Create entity for code

---
+++ src/Entity/PromoCode.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\PromoCodeRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=PromoCodeRepository::class)
+ */
+class PromoCode
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $code;
+
+    /**
+     * @ORM\Column(type="decimal", precision=10, scale=0)
+     */
+    private $Amount;
+
+    /**
+     * @ORM\Column(type="decimal", precision=10, scale=0)
+     */
+    private $RestAmount;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getCode(): ?string
+    {
+        return $this->code;
+    }
+
+    public function setCode(string $code): self
+    {
+        $this->code = $code;
+
+        return $this;
+    }
+
+    public function getAmount(): ?string
+    {
+        return $this->Amount;
+    }
+
+    public function setAmount(string $Amount): self
+    {
+        $this->Amount = $Amount;
+
+        return $this;
+    }
+
+    public function getRestAmount(): ?string
+    {
+        return $this->RestAmount;
+    }
+
+    public function setRestAmount(string $RestAmount): self
+    {
+        $this->RestAmount = $RestAmount;
+
+        return $this;
+    }
+}
