e05a306b6178ce7121a72d87438f3aaa2123a195
Tutt-J <contact@digistos.com>
2020-12-07 17:56:03 +0100

    add field to register event form

--- src/Form/EventType.php
+++ src/Form/EventType.php
@@ -6,6 +6,7 @@ use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
@@ -17,6 +18,11 @@ class EventType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
+            ->add('promoCode', TextType::class, [
+                'label' => 'Code promotionnel ou carte cadeau',
+                'mapped' => false,
+                'required' => false
+            ])
             ->add('agreeTerms', CheckboxType::class, [
                 'label' => 'J\'ai lu et j\'accepte les <a href="/conditions-particulieres-pour-les-evenements-et-sejours">conditions particulières pour les évènements et séjours</a><span class="text-danger"> *</span>' ,
                 'label_html' => true,
