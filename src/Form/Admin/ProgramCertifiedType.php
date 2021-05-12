4f28069e0c2927ff7d76c1c3ddaab866af78bca0
Tutt-J <contact@digistos.com>
2021-05-12 07:57:34 +0200

    resolve error on form for certified

--- src/Form/Admin/ProgramCertifiedType.php
+++ src/Form/Admin/ProgramCertifiedType.php
@@ -2,15 +2,11 @@
 
 namespace App\Form\Admin;
 
-use App\Entity\Address;
-use App\Entity\Program;
+
 use App\Entity\ProgramCertified;
-use App\Repository\AddressRepository;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use App\Form\ImageType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
-use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
-use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
